e83032f22e26e3098ce950955469d956df3eb1ce
ksh5324 <yolly050101@naver.com>
2021-08-25 11:47:43 +0900

    첫 예제

---
+++ 12-04.php
@@ -0,0 +1,7 @@
+<?php
+    $a = 123; echo gettype($a); echo "<br />";
+    $a = 123.123; echo gettype($a); echo "<br />";
+    $a = "MySQL"; echo gettype($a); echo "<br />";
+    $a = true; echo gettype($a); echo "<br />";
+    $a = array(1,2,3); echo gettype($a); echo "<br />";
+?>
